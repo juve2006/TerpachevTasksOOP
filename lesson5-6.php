2efdc59e7620a00dae6389543f79fb07402d6dcd
Evgen Sikora <arnika200@gmail.com>
2022-02-01 16:25:57 +0200

    t1-t6 done

--- lesson5-6.php
+++ lesson5-6.php
@@ -4,15 +4,6 @@ class User
     public string $name;
     public int $age;
 
-    public function isAgeCorrect($age): int
-    {
-        if ($age > 18 && $age < 60) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
     public function setAge($age): int
     {
         if($this->isAgeCorrect($age)){
@@ -41,10 +32,41 @@ class User
             return $this->age;
         }
     }
+
+    private function isAgeCorrect($age): int
+    {
+        return $age >= 18 && $age < 60;
+    }
 }
 
 $user1 = new User();
 $user1->age = 55;
-echo $user1->setAge(17);
-echo $user1->addAge(3);
-echo $user1->subAge(50);
+//echo $user1->setAge(17);
+//echo $user1->addAge(3);
+//echo $user1->subAge(50);
+
+class Student
+{
+    public string $name;
+    public int $course;
+
+    public function transferToNextCourse(): int
+    {
+        if ($this->isCourseCorrect($this->course)) {
+             $this->course = $this->course + 1;
+        }
+        return $this->course;
+    }
+    private function isCourseCorrect($course): int
+    {
+        return $course < 5;
+    }
+}
+
+$student1 = new Student();
+//echo $student1->course = 3;
+//echo '<br>';
+//echo $student1->transferToNextCourse();
+//echo '<br>';
+//echo $student1->transferToNextCourse();
+//echo $student1->transferToNextCourse();
